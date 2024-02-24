1c5c7c09f4a949207bb5dc59757253730c8c4588
suryasai <suryasaigopalam@gmail.com>
2024-02-24 20:46:49 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by surya sai on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
