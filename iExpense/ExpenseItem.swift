a0a3abdfc23d5563e0daaaf69415f24cb2dd58da
suryasai <suryasaigopalam@gmail.com>
2024-05-10 21:45:10 +0530

    sample2222

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,8 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by surya sai on 10/05/24.
+//
+
+import Foundation
---
+++ iExpense/ExpenseItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseItemsView.swift
+//  iExpense
+//
+//  Created by surya sai on 10/05/24.
+//
+
+import SwiftUI
+
+struct ExpenseItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseItemsView()
+}
